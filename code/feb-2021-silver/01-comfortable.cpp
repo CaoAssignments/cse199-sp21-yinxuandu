759571dc1fc3e55115ab71c6d838f977117e95f3
didyxdi <y5du@ucsd.edu>
2021-04-16 07:47:53 -0700

    P1 done

--- code/feb-2021-silver/01-comfortable.cpp
+++ code/feb-2021-silver/01-comfortable.cpp
@@ -4,14 +4,27 @@
 using namespace std;
 
 int n;
-int isvisited[1005][1005];    // 0 means there is no cow in the grid
-                              // 1 means there is cow and it is orginially in the pasture
-                              // 2 means there is cow but it is later added to avoid "comfort"
-                              // 4 means cow has 4 neighbors
+int isvisited[3005][3005];    // 0 means there is no cow in the grid
+                              // 1 means there is cow
 int dx[4] = {0, 0, 1, -1};    // check four directions for each grid
 int dy[4] = {1, -1, 0, 0};
 
 typedef pair<int, int> P;
+
+P need_add(int x, int y) {
+    if (isvisited[x][y] == 0) return P(-1, -1); // remember first to check if 
+                                                // there is cow in the grid
+    int near_num = 0;
+    P ans;
+    for (int k = 0; k < 4; k++) {
+        int nearx = x + dx[k];
+        int neary = y + dy[k];
+        near_num += isvisited[nearx][neary];
+        if (isvisited[nearx][neary] == 0) ans = P(nearx, neary);
+    }
+    if (near_num == 3) return ans;
+    return P(-1, -1);
+}
 
 int main() {
 
@@ -19,60 +32,28 @@ int main() {
 
     queue<P> q;
     int x, y;
-    int ans = 0;
+    int sum = 0; // total num of cows
 
     for (int i = 1; i <= n; i++) {
         scanf("%d%d", &x, &y);
-        x++; y++;              // row/column of added cow could be -1
-        if (isvisited[x][y] == 2) {
-            ans--;
-            isvisited[x][y] = 1;
-        } else {
-            isvisited[x][y] = 1; // ?
+        x+=1000; y+=1000;  // row/column of added cow could be negative
+        q.push(P(x, y));
 
-            q.push(P(x, y));
+        while (!q.empty()) {
+            P tmp = q.front(); q.pop();
+            int ux = tmp.first;
+            int uy = tmp.second;
+            if (isvisited[ux][uy] == 1) continue;
+            sum++; isvisited[ux][uy] = 1;
+            
+            P v = need_add(ux, uy);
+            if (v.first != -1) q.push(v);
             for (int k = 0; k < 4; k++) {
-                int nearx = x + dx[k];
-                int neary = y + dy[k];
-                if (nearx < 0 || neary < 0) continue;
-                q.push(P(nearx, neary));
-            }
-
-            while (!q.empty()) {
-                P tmp = q.front(); q.pop();
-                int ux = tmp.first;
-                int uy = tmp.second;
-                int near_num = 0;  // number of cows near (ux, uy)
-                int blankx, blanky;
-
-                for (int k = 0; k < 4; k++) {
-                    int nearx = ux + dx[k];
-                    int neary = uy + dy[k];
-                    if (nearx < 0 || neary < 0) continue;
-                    if (isvisited[nearx][neary] != 0) near_num++;
-                    else {
-                        blankx = nearx;
-                        blanky = neary;
-                    }
-                }
-
-                if (near_num == 4) isvisited[ux][uy] = 4;
-
-                if (near_num == 3) {
-                    ans++;
-                    isvisited[blankx][blanky] = 2;
-                    q.push(P(blankx, blanky));
-                    for (int k = 0; k < 4; k++) {
-                        int nearx = blankx + dx[k];
-                        int neary = blanky + dy[k];
-                        if (nearx < 0 || neary < 0) continue;
-                        if (isvisited[nearx][neary] == 4) continue;
-                        q.push(P(nearx, neary));
-                    }
-                }
+                v = need_add(ux + dx[k], uy + dy[k]);
+                if (v.first != -1) q.push(v);
             }
         }
-        printf("%d\n", ans);
+        printf("%d\n", sum - i);
     }
     return 0;
 }
