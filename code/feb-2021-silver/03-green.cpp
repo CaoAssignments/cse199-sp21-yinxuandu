bb80f21aa936794c514519afbd50ab46b5d3d564
didyxdi <y5du@ucsd.edu>
2021-04-16 15:09:13 -0700

    P3 done

--- code/feb-2021-silver/03-green.cpp
+++ code/feb-2021-silver/03-green.cpp
@@ -7,6 +7,7 @@ int n;
 int a[505][505];
 int val[505]; // minimum value of the current width
 int sub[505]; // value >=100 from sub[i] to i; -1 if current value <100
+//int lst[505]; // lst[i] is the index of last 100 appear i
 long long ans;
 
 int main() {
@@ -17,7 +18,7 @@ int main() {
         }
     }
 
-    for (int lo = 0; lo < n - 1; lo++) {
+    for (int lo = 0; lo < n; lo++) {
         // initialize val starting from row lo
         for (int j = 0; j < n; j++) val[j] = a[lo][j];
 
@@ -38,21 +39,40 @@ int main() {
                 }
             }
 
+            // debugging
+ //           cout << "lo=" << lo << "\t" << "up=" << up << endl;
+  //          for (int j = 0; j < n; j++) cout << sub[j] << " ";
+
+            long long tmp = 0;
+            int lst = -1; // the index of last 100 by far
+            for (int j = 0; j < n; j++) {
+                if (val[j] == 100) lst = j;
+                if (lst != -1 && sub[j] != -1 && sub[j] <= lst) {
+                    tmp += (long long)(lst - sub[j] + 1);
+ //                   cout << "***" << lst << ", " << sub[j] << ", " << j << endl;
+                }
+            }
+//            cout << endl << "tmp=" << tmp << endl << endl << endl;
+/*
             long long tmp = 0; //
             int k = 0; // iterator
             for (int j = 0; j < n; j++) {
                 if (k >= n) break;
                 if (val[j] != 100) continue;
 
+                if (k < j) k = j;
                 while (k < n) {
-                    if (sub[k] > j) break;
-                    if (sub[k] != -1) tmp += (long long)(k - sub[k] + 1);
+                    if (sub[k] > j || sub[k] == -1) break;
+                    if (sub[k] <= j) {
+                        tmp += (long long)(j - sub[k] + 1);
+                        cout << "***" << sub[k] << ", " << j << endl;
+                    }
                     k++;
                 }
             }
-
+            cout << endl << "tmp=" << tmp << endl << endl << endl;
             //cout << lo << ", " << up << "\t" << tmp << endl;
-
+*/
             ans += tmp; //
         }
     }
@@ -60,5 +80,22 @@ int main() {
     return 0;
 }
 
+// ./03-green < 0.in > 0.out
+
 // ./03-green < testdata/prob3_silver_feb21/1.in > 1real.out
 // diff 1real.out testdata/prob3_silver_feb21/1.out 
+
+// ./03-green < testdata/prob3_silver_feb21/2.in > 2real.out
+// diff 2real.out testdata/prob3_silver_feb21/2.out 
+
+// ./03-green < testdata/prob3_silver_feb21/10.in > 10real.out
+// diff 10real.out testdata/prob3_silver_feb21/10.out 
+
+/*
+5
+101 100 99 102 100
+101 56 100 101 107
+101 100 78 99 106
+100 102 103 104 105
+101 102 70 100 99
+*/
